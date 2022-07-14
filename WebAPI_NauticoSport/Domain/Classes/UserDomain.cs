13af56838504347d8df1c6ef81dafc353b458f32
RandomJuan <juanmanuelmuller@gmail.com>
2022-07-14 02:15:48 -0300

    Features incoming

---
+++ WebAPI_NauticoSport/Domain/Classes/UserDomain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+
+namespace Domain.Classes
+{
+    internal class UserDomain
+    {
+        #region singleton
+        private static UserDomain _instance = new UserDomain();
+
+        private UserDomain() { }
+
+        public static UserDomain Instance()
+        {
+            return _instance;
+        }
+        #endregion
+
+        public IActionResult getFeaturesByUser()
+{
+
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            return Ok(identity);
+        }
+    }
+}
---
+++ WebAPI_NauticoSport/WebAPI_NauticoSport/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebAPI_NauticoSport.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        private static IConfiguration _configuration;
+        private Domain.DomainFacade domainFacade = new Domain.DomainFacade(_configuration);
+
+        public UserController(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        [HttpGet("features")]
+        public IActionResult getFeaturesByUser() {
+
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            return Ok(identity);
+        }
+    }
+}
